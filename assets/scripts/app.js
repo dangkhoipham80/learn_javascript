3dd0379da60b5072dc364398db8f246840c258c8
Phạm Đăng Khôi <dangkhoipham80@gmail.com>
2025-09-04 15:21:18 +0700

    feat: finish ass 1

---
+++ assets/scripts/app.js
@@ -0,0 +1,12 @@
+const defaultResault = 0;
+let currentResault = defaultResault;
+
+currentResault = ((currentResault + 10) * 3) / 2 - 1;
+
+let calcylationDescription = `( ${defaultResault} + 10 ) * 3 / 2 - 1`;
+console.log(calcylationDescription);
+
+let myOtherName;
+const myName = "Khoi";
+myName = "khoi";
+alert(myName);
